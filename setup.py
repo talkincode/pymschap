0a1bd4e4e4f0df5b5e8085985c4abbd76e7cdcdc
wangjuntao <wjt@toughstruct.com>
2017-11-19 12:17:39 +0800

    setup

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='pymschap',
-    version='1.0',
+    version='0.1',
     packages=['pymschap'],
     url='https://github.com/talkincode/pymschap',
     license='LGPL',
